092c36f92b14715b027ad37583f0bd7bad7222ac
egeerdogan <ege@egeerdogan.com>
2019-04-23 14:25:17 +0300

    minor refactoring, added gitignore

--- app/scrapers/sd_scraper.rb
+++ app/scrapers/sd_scraper.rb
@@ -1,33 +1,33 @@
 class SDScraper
-	BASE_URL = "https://slashdot.org"
+  BASE_URL = "https://slashdot.org"
 
-	def self.get_posts(page = 0)
-		doc = Nokogiri::HTML(open("#{BASE_URL}/?page=#{page}"))
+  def self.get_posts(page = 0)
+    doc = Nokogiri::HTML(open("#{BASE_URL}/?page=#{page}"))
 
-		raw_stories = doc.css(".story-title a")
-		raw_comment_counts = doc.css(".comment-bubble")
+    raw_stories = doc.css('.story-title a')
+    raw_comment_counts = doc.css('.comment-bubble')
 
-		titles = []
-		sd_urls = []
+    titles = []
+    sd_urls = []
 
-		comment_counts = raw_comment_counts.map { |c| c.inner_text }
+    comment_counts = raw_comment_counts.map(&:inner_text)
 
-		raw_stories.each_with_index do |story, index| 
-			if !story[:onclick].nil?
-				titles.push story.inner_text
-				sd_urls.push "https:#{story[:href]}"
-			end
-		end
+    raw_stories.each do |story|
+      unless story[:onclick].nil?
+        titles.push story.inner_text
+        sd_urls.push "https:#{story[:href]}"
+      end
+    end
 
-		posts = []
-		titles.each_with_index do |title, index|
-			post = SDPost.new(title,
-											  sd_urls[index],
-											  comment_counts[index])
-			posts.push post
-		end
+    posts = []
+    titles.each_with_index do |title, index|
+      post = SDPost.new(title,
+                        sd_urls[index],
+                        comment_counts[index])
+      posts.push post
+    end
 
-		return posts
-	end
+    return posts
+  end
 
 end
