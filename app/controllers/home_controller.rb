0bbd2745e22ea32586bac22123bcadd977a5f8c0
egeerdogan <ege@egeerdogan.com>
2019-04-25 15:43:39 +0300

    reddit scraper pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
   	@hn_posts = HNScraper.get_posts(25)
   	@sd_posts = SDScraper.get_posts(25)
-		@reddit_posts = RedditScraper.get_posts('programming', 25)
+		@reddit_posts = RedditScraper.get_posts('programming', 40)
 	end
 
 end
--- app/scrapers/reddit_scraper.rb
+++ app/scrapers/reddit_scraper.rb
@@ -1,23 +1,36 @@
 class RedditScraper
   BASE_URL = 'https://www.reddit.com/r'
+  ARTICLES_PER_PAGE = 25 # reddit default
 
   def self.get_posts(subreddit, count = 15)
+
+    page_count = (count / 25.0).ceil
+
     json = HTTParty.get("#{BASE_URL}/#{subreddit}/.json?count=#{count}",
                         headers: {'User-Agent' => 'news-app'})
-    content = JSON.parse json.to_s, symbolize_names: true
 
     coder = HTMLEntities.new
 
     posts = []
-    (0...count).each do |i|
-      data = content[:data][:children][i][:data]
-      post = RedditPost.new(coder.decode(data[:title]),
-                            data[:num_comments],
-                            data[:ups],
-                            data[:url],
-                            data[:permalink],
-                            data[:domain])
-      posts.push post
+
+    page_count.times do |page|
+      content = JSON.parse json.to_s, symbolize_names: true
+      after = content[:data][:after]
+
+      ARTICLES_PER_PAGE.times do |i|
+        data = content[:data][:children][i][:data]
+        post = RedditPost.new(coder.decode(data[:title]),
+                              data[:num_comments],
+                              data[:ups],
+                              data[:url],
+                              data[:permalink],
+                              data[:domain])
+        posts.push post
+        break if posts.length == count
+      end
+
+      json = HTTParty.get("#{BASE_URL}/#{subreddit}/.json?count=#{count}&after=#{after}",
+                        headers: {'User-Agent' => 'news-app'})
     end
 
     return posts
