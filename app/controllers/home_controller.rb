c3a6e19d4d77bd1bb057bc9e4bdd83907e2358e1
egeerdogan <ege@egeerdogan.com>
2019-04-26 23:11:06 +0300

    posts limit, comments wording

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
 
+	LIMIT = 25
+
   def index
-  	@hn_posts = HNScraper.get_posts(25)
-  	@sd_posts = SDScraper.get_posts(25)
-		@reddit_posts = RedditScraper.get_posts('programming', 25)
+  	@hn_posts = HNScraper.get_posts(LIMIT)
+  	@sd_posts = SDScraper.get_posts(LIMIT)
+		@reddit_posts = RedditScraper.get_posts('programming', LIMIT)
 	end
 
 end
