7e4574dfa4a63d3a906b7266598b9a51dc1978a5
egeerdogan <ege@egeerdogan.com>
2019-04-27 17:17:55 +0300

    benchmarking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,21 @@
 class HomeController < ApplicationController
+	require 'benchmark'
 
 	LIMIT = 25
 
   def index
-  	@hn_posts = HNScraper.get_posts(LIMIT)
-  	@sd_posts = SDScraper.get_posts(LIMIT)
-		@reddit_posts = RedditScraper.get_posts('programming', LIMIT)
+  	puts "Hacker News: "
+  	puts Benchmark.measure { 
+  		@hn_posts = HNScraper.get_posts(LIMIT)
+  	}
+  	puts "Slashdot: "
+  	puts Benchmark.measure {
+  		@sd_posts = SDScraper.get_posts(LIMIT)
+  	}
+  	puts "Reddit: "
+		puts Benchmark.measure {
+			@reddit_posts = RedditScraper.get_posts('programming', LIMIT)
+		}
 	end
 
 end
