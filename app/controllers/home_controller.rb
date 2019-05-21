048200e604735b6cc61241ccc3af4083a8c01875
egeerdogan <ege@egeerdogan.com>
2019-05-21 18:01:52 +0300

    sd post <- summary

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,11 @@ class HomeController < ApplicationController
   def index
   	@hn_posts = HNScraper.get_posts(LIMIT)
   	@sd_posts = SDScraper.get_posts(LIMIT)
+
+  	puts
+  	pp @sd_posts[0].summary
+  	puts
+
   	@reddit_posts = RedditScraper.get_posts('programming', LIMIT)
 	end
 
