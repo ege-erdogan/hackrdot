7914c82dc1e2de2ab4b6acb88db15708194bc14a
egeerdogan <ege@egeerdogan.com>
2019-06-13 10:37:26 +0300

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do	
 
+  get 'sessions/new'
 	root 'home#index'
 
 	get '/signup', to: 'users#new'
 	post '/signup', to: 'users#create'
+
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
+	get '/logout', to: 'sessions#destroy'
+
 	get '/users', to: 'users#show'
 
 end
