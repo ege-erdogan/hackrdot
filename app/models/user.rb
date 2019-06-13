27c5c524e440f7a8a8095ed28fad89f90e55f8ab
egeerdogan <ege@egeerdogan.com>
2019-06-13 11:09:15 +0300

    remember users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+	attr_accessor :remember_token
+
 	has_secure_password
 
 	before_save { email.downcase! }
@@ -11,6 +13,30 @@ class User < ApplicationRecord
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
 	validates :password, presence: true,
-											 length: { minimum: 6 }									
+											 length: { minimum: 6 }		
+
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+																									BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end							
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	def forget 
+		update_attribute(:remember_digest, nil)
+	end
+
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
 
 end	
