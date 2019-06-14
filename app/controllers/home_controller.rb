14dd889fbde8474c443fecc680aababab01eca92
egeerdogan <ege@egeerdogan.com>
2019-06-14 23:20:46 +0300

    hide save button on already saved posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,9 @@ class HomeController < ApplicationController
     @hn_posts = HNScraper.get_posts(LIMIT)
     @sd_posts = SDScraper.get_posts
     @reddit_posts = RedditScraper.get_posts('programming', LIMIT)
+    if logged_in?
+    	@saved_posts = current_user.bookmarks.map(&:comments_url)
+    end
   end
 
 end
