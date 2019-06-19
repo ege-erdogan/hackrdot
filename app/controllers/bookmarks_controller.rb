3b723e77dcbd678188df7717bdeea7a628e3f7b5
egeerdogan <ege@egeerdogan.com>
2019-06-19 11:58:51 +0300

    can delete bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,30 +10,15 @@ class BookmarksController < ApplicationController
 		end
 	end
 
-	def create
+	def handle
 		comments_url = params[:comments_url]
 		title = params[:title]
 		user_id = params[:user_id]
 
-		bookmark = Bookmark.find_by(comments_url: comments_url)
-		if !bookmark
-			comments_url.prepend 'https:' if comments_url.include? 'slashdot.org/'
-			bookmark = Bookmark.new(title: title, comments_url: comments_url)
-		end
-
-		user = User.find_by(id: user_id)
-		user.bookmarks << bookmark unless user.bookmarks.include? bookmark
-	end
-
-	def destroy
-		user_id = params[:user_id]
 		if logged_in? && current_user.id == user_id
-			user = User.find_by(id: user_id)
-			comments_url = params[:comments_url]
-			comments_url.prepend 'https:' if comments_url.include? 'slashdot.org/'
-			user.bookmarks.where(comments_url: params[:comments_url]).destroy_all
-		else 
-			flash[:danger] = 'Make sure you are logged in and not doing something strange.'
+			handle_bookmark(user_id, comments_url, title)
+		else
+			flash[:danger] = 'Make sure you are logged in.'
 			redirect_to root_path
 		end
 	end
--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,3 +1,29 @@
 module BookmarksHelper
 
+	def create_bookmark(user, bookmark)
+		puts user.id
+		user.bookmarks << bookmark
+	end
+
+	def delete_bookmark(user, bookmark)
+		user.bookmarks.where(comments_url: bookmark.comments_url).destroy_all
+	end
+
+	def handle_bookmark(user_id, comments_url, title)
+		comments_url.prepend 'https:' if comments_url.include? 'slashdot.org/'
+
+		bookmark = Bookmark.find_by(comments_url: comments_url)
+		if bookmark.nil?
+			bookmark = Bookmark.new(title: title, comments_url: comments_url)
+		end
+
+		user = User.find_by(id: user_id)
+
+		if user.bookmarks.include? bookmark
+			delete_bookmark(user, bookmark)
+		else
+			create_bookmark(user, bookmark)
+		end
+	end
+
 end
