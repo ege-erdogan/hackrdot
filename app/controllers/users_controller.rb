576da25e9c9d829b471af177dc4e9d5a05653da6
egeerdogan <ege@egeerdogan.com>
2019-06-26 11:13:40 +0300

    add subscribe checkbox to signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 	private 
 
 		def user_params
-			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+			params.require(:user).permit(:name, :email, :password, :password_confirmation, :subscribed)
 		end
 
 end
