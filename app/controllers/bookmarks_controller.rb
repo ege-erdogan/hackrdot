cd4fc464e8db95b9caa4232533a2d2eefb11d348
egeerdogan <ege@egeerdogan.com>
2019-06-28 18:40:29 +0300

    remove user id from url for seeing bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,10 @@
 class BookmarksController < ApplicationController
 
 	def show
-		user_id = params[:user_id]
-		site = params[:site]
+		if logged_in?
+			user_id = session[:user_id]
+			site = params[:site]
 
-		if logged_in? && current_user.id == user_id
 			@bookmarks = User.find_by(id: user_id).bookmarks.reverse
 			if !site.nil?
 				@bookmarks.filter!{ |bookmark| bookmark.from_domain? site }
@@ -17,12 +17,12 @@ class BookmarksController < ApplicationController
 	end
 
 	def handle
-		comments_url = params[:comments_url]
-		title = params[:title]
-		user_id = params[:user_id]
-		source = params[:source]
+		if logged_in?
+			comments_url = params[:comments_url]
+			title = params[:title]
+			user_id = session[:user_id]
+			source = params[:source]
 
-		if logged_in? && current_user.id == user_id
 			handle_bookmark(user_id, comments_url, title)
 			if source == 'bookmarks'
 				redirect_to show_bookmarks_path(user_id: user_id)	
